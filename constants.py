faa34cf92e9bfa4c1b7a56ae7c0bbb008bb34bee
Michael Teolis <michaelteolis96@gmail.com>
2022-07-05 18:54:03 -0400

    Add constants file

---
+++ constants.py
@@ -0,0 +1,29 @@
+"""
+constants.py
+"""
+
+URL_KEY = 'URL'
+PRICE_KEY = 'PRICE'
+RAM_KEY = 'RAM'
+NAME_KEY = 'NAME'
+
+CANAKIT_KEY = 'canakit.com'
+CANADA_NEWARK_KEY = 'canada.newark.com'
+DIGIKEY_KEY = 'digikey.ca'
+PI_URLS = {
+    CANAKIT_KEY: [
+        'https://www.canakit.com/raspberry-pi-4-2gb.html',
+        'https://www.canakit.com/raspberry-pi-4-4gb.html',
+        'https://www.canakit.com/raspberry-pi-4-8gb.html'
+    ],
+    CANADA_NEWARK_KEY: [
+        'https://canada.newark.com/raspberry-pi/rpi4-modbp-8gb/raspberry-pi-4-model-b-8gb/dp/64AH2041',
+        'https://canada.newark.com/raspberry-pi/rpi4-modbp-4gb/raspberry-pi-4-model-b-4gb-rohs/dp/02AH3164',
+        'https://canada.newark.com/raspberry-pi/rpi4-modbp-2gb/raspberry-pi-4-model-b-2gb-rohs/dp/02AH3162'
+    ],
+    DIGIKEY_KEY: [
+        'https://www.digikey.ca/en/products/detail/raspberry-pi/RASPBERRY-PI-4-MODEL-B-8G/12159401',
+        'https://www.digikey.ca/en/products/detail/raspberry-pi/Raspberry-Pi-4B-4GB/10258781',
+        'https://www.digikey.ca/en/products/detail/raspberry-pi/Raspberry-Pi-4B-2GB/10258782'
+    ]
+}
