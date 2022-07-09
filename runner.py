5129f36cedbe9508d2bdc55f6162c873ca24a399
Michael Teolis <michaelteolis96@gmail.com>
2022-07-09 02:34:55 -0400

    Add digikey parsing to runner

--- runner.py
+++ runner.py
@@ -1,9 +1,12 @@
 from pishop import Pishop
 from canakit import Canakit
+from digikey import Digikey
 
 print(f'running scraper...')
 # pishop = Pishop()
 # pishop.parse()
-canakit = Canakit()
-canakit.parse()
+# canakit = Canakit()
+# canakit.parse()
+digikey = Digikey()
+digikey.parse()
 print(f'running scraper complete')
