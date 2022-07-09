8cca9110558869af9fa4771cbfd059d545aef956
Michael Teolis <michaelteolis96@gmail.com>
2022-07-09 02:34:27 -0400

    Move digikey values to digikey

--- constants.py
+++ constants.py
@@ -8,16 +8,10 @@ RAM_KEY = 'RAM'
 NAME_KEY = 'NAME'
 
 CANADA_NEWARK_KEY = 'canada.newark.com'
-DIGIKEY_KEY = 'digikey.ca'
 PI_URLS = {
     CANADA_NEWARK_KEY: [
         'https://canada.newark.com/raspberry-pi/rpi4-modbp-8gb/raspberry-pi-4-model-b-8gb/dp/64AH2041',
         'https://canada.newark.com/raspberry-pi/rpi4-modbp-4gb/raspberry-pi-4-model-b-4gb-rohs/dp/02AH3164',
         'https://canada.newark.com/raspberry-pi/rpi4-modbp-2gb/raspberry-pi-4-model-b-2gb-rohs/dp/02AH3162'
-    ],
-    DIGIKEY_KEY: [
-        'https://www.digikey.ca/en/products/detail/raspberry-pi/RASPBERRY-PI-4-MODEL-B-8G/12159401',
-        'https://www.digikey.ca/en/products/detail/raspberry-pi/Raspberry-Pi-4B-4GB/10258781',
-        'https://www.digikey.ca/en/products/detail/raspberry-pi/Raspberry-Pi-4B-2GB/10258782'
     ]
 }
